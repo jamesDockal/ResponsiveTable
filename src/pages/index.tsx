0dd2f3224b4f4246879a98c46706641137494b2d
Dockal <jamesistranho@gmail.com>
2022-07-15 17:02:11 -0300

    feat: final home page and global css variables

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,133 @@
 import type { NextPage } from "next";
 import styles from "../../styles/Home.module.css";
+import { Card } from "../components/Card";
+import { Modal } from "../components/Modal";
 import { Table } from "../components/Table";
 
 const Home: NextPage = () => {
   const elements = [
     {
-      position: 6,
-      mass: 12.011,
-      symbol: <strong>Symbol</strong>,
-      name: "Carbon",
+      id: 1,
+      name: "James",
+      email: "james@email.com",
+      salary: "$80,000",
+      Documents: [
+        <Card
+          key={10}
+          imgSrc="/images/Text.jpg"
+          href={{
+            link: "#",
+            title: "Offer.pdf",
+          }}
+        />,
+        <Card
+          key={11}
+          imgSrc="/images/Text.jpg"
+          href={{
+            link: "#",
+            title: "Employment.pdf",
+          }}
+        />,
+        <Card
+          key={12}
+          imgSrc="/images/Text.jpg"
+          href={{
+            link: "#",
+            title: "Assigment.pdf",
+          }}
+        />,
+      ],
     },
-    { position: 7, mass: 14.007, symbol: "N", name: "Nitrogen" },
-    { position: 39, mass: 88.906, symbol: "Y", name: "Yttrium" },
-    { position: 56, mass: 137.33, symbol: "Ba", name: "Barium" },
     {
-      position: 58,
-      mass: 140.12,
-      symbol: "Ce",
-      name: (
-        <>
-          <div>ppppppppppppppppppppp</div>
-          <div>ppppppppppppppppppppp</div>
-          <div>ppppppppppppppppppppp</div>
-          <div>ppppppppppppppppppppp</div>
-        </>
-      ),
+      id: 2,
+      name: "Person1",
+      email: "email@email.com",
+      salary: "$00,000",
+      Documents: [
+        <Card
+          key={1}
+          imgSrc="/images/Text.jpg"
+          href={{
+            link: "#",
+            title: "Offer.pdf",
+          }}
+        />,
+      ],
+    },
+    {
+      id: 3,
+      name: "Person2",
+      email: "email@email.com",
+      salary: "$00,000",
+      Documents: [
+        <Card
+          key={2}
+          imgSrc="/images/Text.jpg"
+          href={{
+            link: "#",
+            title: "Offer.pdf",
+          }}
+        />,
+        <Card
+          key={3}
+          imgSrc="/images/Text.jpg"
+          href={{
+            link: "#",
+            title: "Offer.pdf",
+          }}
+        />,
+      ],
+    },
+    {
+      id: 4,
+      name: "Person3",
+      email: "email@email.com",
+      salary: "$00,000",
+      Documents: [
+        <Card
+          key={4}
+          imgSrc="/images/Text.jpg"
+          href={{
+            link: "#",
+            title: "Offer.pdf",
+          }}
+        />,
+        <Card
+          key={5}
+          imgSrc="/images/Text.jpg"
+          href={{
+            link: "#",
+            title: "Employment.pdf",
+          }}
+        />,
+        <Card
+          key={6}
+          imgSrc="/images/Text.jpg"
+          href={{
+            link: "#",
+            title: "Assigment.pdf",
+          }}
+        />,
+        <Card
+          key={7}
+          imgSrc="/images/Text.jpg"
+          href={{
+            link: "#",
+            title: "Employment.pdf",
+          }}
+        />,
+        <Card
+          key={8}
+          imgSrc="/images/Text.jpg"
+          href={{
+            link: "#",
+            title: "Assigment.pdf",
+          }}
+        />,
+      ],
     },
   ];
+
   return (
     <div>
       <Table rows={elements} />
